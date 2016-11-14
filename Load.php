5bee28b260ce36a44fedf349ed83fbef9572867d
Ivansap <kartoshka95@gmail.com>
2016-11-14 17:52:56 +0500

    LOAD без ошибок

--- Load.php
+++ Load.php
@@ -34,11 +34,11 @@
                         if ($xr->nodeType == XMLReader::END_ELEMENT && $xr->localName == "row") break;
                         if ($xr->nodeType == XMLReader::ELEMENT)
                         {
-                            $key = intval(substr($xr->localName, 1);
+                            $key = intval(substr($xr->localName, 1));
                         }
                         if ($xr->nodeType == XMLReader::TEXT)
                         {
-                            $arr[$n, $key] = $xr->value;
+                            $arr[$n][$key] = $xr->value;
                         }
                     }
                 }
@@ -47,27 +47,27 @@
             $cnt = 1;
             for ($i = 0; $i < $n; $i++)
             {
-                if ($arr[$i, 6] == null) $arr[$i, 6] = "2";
-                $sem = intval($arr[$i, 6]);
+                if ($arr[$i][6] == null) $arr[$i][6] = "2";
+                $sem = intval($arr[$i][6]);
                 if (($sem % 2 == 1) != $isAutumn) continue;
 
-                $names[$cnt] = $arr[$i, 1];
-                $spect[$cnt] = $arr[$i, 3];
-                $courses[$cnt] = $arr[$i, 5];
+                $names[$cnt] = $arr[$i][1];
+                $spect[$cnt] = $arr[$i][3];
+                $courses[$cnt] = $arr[$i][5];
 
                 if ($spect[$cnt] == null)
-                    $spect[$cnt] = $arr[$i, 4];
+                    $spect[$cnt] = $arr[$i][4];
                 for ($j = 1; $j <= $countOfTypes; $j++)
                 {
-                    if ($arr[$i, $j + 4] == null || strlen($arr[$i, $j + 4]) == 0)
-                        $z[$cnt, $j] = 0;
+                    if ($arr[$i][$j + 4] == null || strlen($arr[$i][$j + 4]) == 0)
+                        $z[$cnt][$j] = 0;
                     else
                     {
                         $res = 0.0;
-                        $arr[$i, $j + 4] = str_replace('.', ',', $arr[$i, $j + 4]);
+                        $arr[$i][$j + 4] = str_replace('.', ',', $arr[$i][$j + 4]);
                         // tut nado tryparse if (floatval($arr[$i, $j + 4])
                         //{
-                            $z[$cnt, $j] = floatval($arr[$i, $j + 4]);
+                            $z[$cnt][$j] = floatval($arr[$i][$j + 4]);
                         //}
                         //else
                         //{
@@ -80,5 +80,5 @@
             $countOfSubjects = $cnt - 1;
         }
     }
-}
+
 ?>
